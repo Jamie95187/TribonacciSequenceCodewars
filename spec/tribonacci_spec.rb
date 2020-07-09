e350fcf07318eff114959110d1f04a568aa9550a
Jamie <jamiewong63@googlemail.com>
2020-07-09 18:43:16 +0100

    test input: with doubles output: correct 30 element array

--- spec/tribonacci_spec.rb
+++ spec/tribonacci_spec.rb
@@ -26,6 +26,10 @@ describe 'tribonacci' do
       expect(tribonacci.present([-2,-2,4],5)).to eq [-2,-2,4,0,2]
     end
 
+    it('should return the correct 30 element array, with doubles') do
+      expect(tribonacci.present([0.5,0.5,0.5],30)).to eq [0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5]
+    end
+
   end
 
 end
